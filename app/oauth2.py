9a1e55553a2176291adb0a42c91ffdbe7d7e7341
Bagus Rexy W <59761891+bagusrexy@users.noreply.github.com>
2023-01-14 01:06:56 +0700

    Add files via upload

---
+++ app/oauth2.py
@@ -0,0 +1,38 @@
+from jose import JWSError, jwt
+from datetime import datetime, timedelta
+from . import schemas
+from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+    return encoded_jwt
+
+def verify_accsess_token(token: str, credential_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = payload.get("user_id")
+
+        if id is None:
+            raise credential_exception
+        token_data = schemas.TOkenData(id=id)
+    except JWSError:
+        raise credential_exception
+    return token_data
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credetials_exception =  HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+
+    return verify_accsess_token(token, credetials_exception)
